cb4a9ef777d2ccf032c3ceedf3a64b0a8bbaf20a
Maksymkvn <kvasnikovmv@gmail.com>
2023-02-15 13:46:46 +0200

    Made CustomerService and CustomerMapper

--- src/main/java/com/bank/account/mapper/CustomerMapper.java
+++ src/main/java/com/bank/account/mapper/CustomerMapper.java
@@ -1,12 +1,40 @@
 package com.bank.account.mapper;
 
+import com.bank.account.mapper.domen.Customer;
+import com.bank.account.mapper.domen.dto.CustomerReqDto;
+import com.bank.account.mapper.domen.dto.CustomerRespDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class CustomerMapper {
 
+    public CustomerRespDto customerToRespDto(Customer customer) {
+        return Optional.ofNullable(customer)
+                .stream()
+                .findAny()
+                .map(c -> CustomerRespDto.builder()
+                        .id(c.getId())
+                        .firstName(c.getFirstName())
+                        .lastName(c.getLastName())
+                        .dateOfBirth(c.getDateOfBirth())
+                        .active(c.isActive())
+                        .build())
+                .orElse(null);
+    }
 
-
+    public Optional<Customer> customerReqDtoToCustomer(CustomerReqDto customerReqDto) {
+        return Optional.ofNullable(customerReqDto)
+                .stream()
+                .findAny()
+                .map(c -> Customer.builder()
+                        .firstName(c.getFirstName())
+                        .lastName(c.getLastName())
+                        .dateOfBirth(c.getDateOfBirth())
+                        .active(c.isActive())
+                        .build());
+    }
 }
--- src/main/java/com/bank/account/service/CustomerServiceImpl.java
+++ src/main/java/com/bank/account/service/CustomerServiceImpl.java
@@ -1,10 +1,10 @@
 package com.bank.account.service;
 
+import com.bank.account.mapper.CustomerMapper;
 import com.bank.account.mapper.domen.dto.CustomerReqDto;
 import com.bank.account.mapper.domen.dto.CustomerRespDto;
 import com.bank.account.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
-import org.apache.el.stream.Stream;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,30 +16,52 @@ import java.util.stream.StreamSupport;
 public class CustomerServiceImpl implements CustomerService {
 
     private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
 
     @Override
     public List<CustomerRespDto> getAll() {
-
-        return null;
+        return StreamSupport.stream(customerRepository.findAll().spliterator(), false)
+                .toList().stream()
+                .map(customerMapper::customerToRespDto)
+                .toList();
     }
 
     @Override
     public Optional<CustomerRespDto> getById(String id) {
-        return Optional.empty();
+        return customerRepository.findById(id)
+                .map(customerMapper::customerToRespDto);
     }
 
     @Override
     public Optional<CustomerRespDto> create(CustomerReqDto customerReqDto) {
-        return Optional.empty();
+        return customerMapper.customerReqDtoToCustomer(customerReqDto)
+                .map(customerRepository::save)
+                .map(customerMapper::customerToRespDto);
     }
 
     @Override
     public Optional<CustomerRespDto> update(String id, CustomerReqDto customerReqDto) {
-        return Optional.empty();
+        return customerRepository.findById(id)
+                .map(c -> {
+                    if (customerReqDto != null) {
+                        c.setFirstName(customerReqDto.getFirstName());
+                        c.setLastName(customerReqDto.getLastName());
+                        c.setDateOfBirth(customerReqDto.getDateOfBirth());
+                        c.setActive(customerReqDto.isActive());
+                        return c;
+                    }
+                    return null;
+                })
+                .map(customerMapper::customerToRespDto);
     }
 
     @Override
     public Optional<CustomerRespDto> delete(String id) {
-        return Optional.empty();
+        Optional<CustomerRespDto> customerRespDto = customerRepository.findById(id)
+                .map(customerMapper::customerToRespDto);
+        if (customerRepository.existsById(id)) {
+            customerRepository.deleteById(id);
+        }
+        return customerRespDto;
     }
 }
